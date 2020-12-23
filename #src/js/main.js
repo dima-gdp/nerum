d3bf5b206ebd874572aa6a77bd64b86e4800f0fe
dima-gdp <dima5620g@gmail.com>
2020-12-23 18:06:01 +0300

    first

---
+++ #src/js/main.js
@@ -0,0 +1,87 @@
+$(document).ready(function () {
+	objectFitImages();
+
+	const searchBtn = $('.header__search-btn');
+
+	searchBtn.on('click', function(){
+		searchBtn.toggleClass('header__search-btn--close');
+		$('.header__form').toggleClass('header__form--active');
+	});
+
+	const slider_hero = new Swiper('.slider-hero', {
+
+		slidesPerView: 1,
+		loop: false,
+		navigation: {
+			nextEl: '.slider-hero__btn-next',
+			prevEl: '.slider-hero__btn-prev',
+		},
+		// breakpoints: {
+		// 	320: {
+		// 		slidesPerView: 1,
+		// 		spaceBetween: 31,
+
+		// 	},
+		// 	769: {
+		// 		slidesPerView: 2,
+		// 		spaceBetween: 25,
+
+		// 	},
+		// 	993: {
+		// 		slidesPerView: 2,
+		// 		spaceBetween: 31,
+		// 	},
+		// }
+	});
+
+
+	// Fancy-box
+	// $(".to-modal").on('click', function () {
+	// 	$.fancybox.open({
+	// 		src: '#modal',
+	// 		touch: 'false',
+	// 		smallBtn: false,
+	// 		buttons: '',
+	// 	});
+	// });
+
+	// $("#to-privacy").on('click', function () {
+	// 	$.fancybox.open({
+	// 		src: '#modal-polit',
+	// 		touch: 'false',
+	// 		smallBtn: false,
+	// 		buttons: '',
+	// 	});
+	// });
+
+	// Input-mask
+	// $('input[type="tel"]').inputmask({ "mask": "+7 (999)-999-99-99" });
+
+	// // Табы
+	// $('ul.tabs__list').on('click', 'li:not(.active)', function () {
+	// 	$(this).addClass('active').siblings().removeClass('active')
+	// 		.closest('div.tabs').find('div.tabs__block').removeClass('active').eq($(this).index()).addClass('active');
+	// })
+
+	// Menu-burger
+	// burger.click(function () {
+	// 	mobMenu.addClass('active')
+	// })
+
+	// $('.mob-menu__close').click(function () {
+	// 	mobMenu.removeClass('active')
+	// })
+
+	// $(document).click(function (ev) {
+	// 	if (!ev.target.closest('.header__burger')) {
+
+	// 		if (!ev.target.closest('.mob-menu')) {
+	// 			mobMenu.removeClass('active')
+	// 		}
+
+	// 	}
+	// })
+
+
+
+});
